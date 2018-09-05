babc6df222e3736224c5d65e5c3f395a034a7893
Nick Frichette <nick@frichetten.com>
2018-09-05 18:08:12 -0500

    Setup the virtualenv and started working on the metric logging

--- dashboard_collector.py
+++ dashboard_collector.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 import logging
 import logging.handlers
@@ -6,6 +6,10 @@ import stem
 from stem.control import Controller
 from stem.util.str_tools import get_size_label
 from datetime import timedelta
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
 
 # Adding Syslog logging
 handler = logging.handlers.SysLogHandler(address = '/dev/log')
@@ -17,25 +21,32 @@ logger.addHandler(handler)
 try:
     controller = Controller.from_port()
 except stem.SockerError as exc:
-    logger.debug('Failed to create controller')
+    logger.debug('[Dashboard] Failed to create controller')
     exit()
 
 # Trying to authenticate
 try:
-    pw = open('/root/controller_password','r').readline()[:-1]
-    controller.authenticate(password = pw)
+    pw = open('./controller_password','r').readline()[:-1]
+    controller.authenticate(password=pw)
 except stem.connection.MissingPassword:
-    logger.debug('Failed to authenticate with control port')
+    print("[-] You need a password to authenticate with Tor")
+    logger.debug('[Dashboard] Failed to authenticate with control port')
     exit()
 
-# Gather data
-bandwidthrate = get_size_label(int(controller.get_conf('RelayBandwidthRate', '0')))
-eff_bandwidthrate = get_size_label(int(controller.get_effective_rate()))
-br = get_size_label(int(controller.get_info("traffic/read")))
-bw = get_size_label(int(controller.get_info("traffic/written")))
-with open('/proc/uptime', 'r') as f:
-    uptime_seconds = float(f.readline().split()[0])
-    uptime = str(timedelta(seconds = uptime_seconds))
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def homepage(path):
+    return render_template('index.html')
 
-# Do stuff
+def get_metrics():
+    bandwidthrate = get_size_label(int(controller.get_conf('RelayBandwidthRate', '0')))
+    eff_bandwidthrate = get_size_label(int(controller.get_effective_rate()))
+    br = get_size_label(int(controller.get_info("traffic/read")))
+    bw = get_size_label(int(controller.get_info("traffic/written")))
+    with open('/proc/uptime', 'r') as f:
+        uptime_seconds = float(f.readline().split()[0])
+        uptime = str(timedelta(seconds = uptime_seconds))
+
+if __name__ == "__main__":
+    app.run(port=5000)
 
